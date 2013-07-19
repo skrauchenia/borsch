83c0919e3a1cd16985704f7d3385d81e5d943f54
AndrewZh <hotsmileband@gmail.com>
2013-07-19 16:25:56 +0300

    Added User, AccessRight and Encoder classes

---
+++ borsch-core/src/main/java/com/exadel/borsch/dao/AccessRight.java
@@ -0,0 +1,9 @@
+package com.exadel.borsch.dao;
+
+public enum AccessRight {
+    EDIT_MENU_SELF,
+    EDIT_MENU_OTHER,
+    EDIT_PRICE,
+    PRINT_ORDER,
+    EDIT_PROFILE
+}
---
+++ borsch-core/src/main/java/com/exadel/borsch/dao/User.java
@@ -0,0 +1,65 @@
+package com.exadel.borsch.dao;
+
+import com.exadel.borsch.util.Encoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class User {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(User.class);
+
+    private String name;
+    private String email;
+    private String passwordHash;
+    private UUID id;
+    private List<AccessRight> accessRights = new ArrayList<>();
+
+    User() {
+        id = UUID.randomUUID();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public boolean addAccessRights(List<AccessRight> toAdd) {
+        return accessRights.addAll(toAdd);
+    }
+
+    public boolean discardAccessRights(List<AccessRight> toDiscard) {
+        return accessRights.removeAll(toDiscard);
+    }
+
+    public String getHash() {
+        String usersHash = Encoder.encodeWithMD5(name, email);
+        return usersHash;
+    }
+}
