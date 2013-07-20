88d4439fd68660c50ff4e55038c6270ab38db8d8
AndrewZh <hotsmileband@gmail.com>
2013-07-20 20:42:26 +0300

    Added missin expirationDate field and getHash() method to PriceList class. Also overriden equals() and hashCode() methods

--- borsch-core/src/main/java/com/exadel/borsch/dao/PriceList.java
+++ borsch-core/src/main/java/com/exadel/borsch/dao/PriceList.java
@@ -1,5 +1,8 @@
 package com.exadel.borsch.dao;
 
+import com.exadel.borsch.util.Encoder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.joda.time.DateTime;
 
 import java.util.ArrayList;
@@ -12,6 +15,15 @@ import java.util.List;
 public class PriceList {
     private List<Dish> dishes = new ArrayList<>();
     private DateTime creationTime;
+    private DateTime expirationTime;
+
+    public DateTime getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(DateTime expirationTime) {
+        this.expirationTime = expirationTime;
+    }
 
     public DateTime getCreationTime() {
         return creationTime;
@@ -31,5 +43,35 @@ public class PriceList {
 
     public boolean removeDishes(List<Dish> toRemove) {
         return dishes.removeAll(toRemove);
+    }
+
+    public String getHash() {
+        StringBuilder forHash = new StringBuilder();
+        for (Dish dish : dishes) {
+            forHash.append(dish.getHashId());
+        }
+
+        return Encoder.encodeWithMD5(expirationTime.toString() + creationTime.toString(),
+                        forHash.toString());
+    }
+
+    @Override
+    public boolean equals(Object toCompare) {
+        if(toCompare instanceof PriceList) {
+            return new EqualsBuilder()
+                    .append(creationTime, ((PriceList) toCompare).getCreationTime())
+                    .append(expirationTime, ((PriceList) toCompare).getExpirationTime())
+                    .append(this.getHash(), ((PriceList) toCompare).getHash())
+                    .isEquals();
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(42, 17)
+                .append(this.getHash())
+                .toHashCode();
     }
 }
