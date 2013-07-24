1c44e0aa48eb9a4b026c2def0ff73530ba074116
AndrewZh <hotsmileband@gmail.com>
2013-07-24 21:32:28 +0300

    Edited userEdit.jsp and UsersController

--- borsch-web/src/main/java/com/exadel/borsch/web/controllers/UsersController.java
+++ borsch-web/src/main/java/com/exadel/borsch/web/controllers/UsersController.java
@@ -1,9 +1,16 @@
 package com.exadel.borsch.web.controllers;
 
+import com.exadel.borsch.dao.User;
+import com.exadel.borsch.managers.ManagerFactory;
+import com.exadel.borsch.managers.UserManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
 
 /**
  *
@@ -11,13 +18,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class UsersController {
+
+    @Autowired
+    private ManagerFactory managerFactory;
     @RequestMapping("/users")
     public String processPageRequest() {
         return ViewURLs.USERS_PAGE;
     }
     @RequestMapping("/edit/user/{userId}")
     public String processEditPageRequest(@PathVariable String userId, ModelMap model) {
-        model.addAttribute("login", userId);
+        UserManager userManager = managerFactory.getUserManager();
+        User user = userManager.getUserById(UUID.fromString(userId));
+        model.addAttribute("user", user);
+        return ViewURLs.USER_EDIT_PAGE;
+    }
+
+    @RequestMapping("/edit/user/{userId}/edit")
+    public String processUpdateUserRequest(@PathVariable String userId, @RequestParam("name") String name,
+                                         @RequestParam("eMail") String email) {
+        UserManager userManager = managerFactory.getUserManager();
+        User user = userManager.getUserById(UUID.fromString(userId));
+        user.setName(name);
+        user.setEmail(email);
+        userManager.updateUser(user);
+
         return ViewURLs.USER_EDIT_PAGE;
     }
 }
--- borsch-web/src/main/webapp/WEB-INF/jsp/userEdit.jsp
+++ borsch-web/src/main/webapp/WEB-INF/jsp/userEdit.jsp
@@ -6,9 +6,28 @@
     <jsp:attribute name="head">
         <title>Edit profile</title>
     </jsp:attribute>
-        
+
     <jsp:body>
         <div class="container">
+            <form class="form-horizontal" action="${contextPath}/edit/user/${user.id}/edit">
+                <div class="control-group">
+                    <label class="control-label" for="name">Name</label>
+                    <div class="controls">
+                        <input type="text" id="name" value="${user.name}">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="eMail">Email</label>
+                    <div class="controls">
+                        <input type="text" id="eMail" value="${user.email}">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="controls">
+                        <button type="submit" class="btn">Submit</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </jsp:body>
         
