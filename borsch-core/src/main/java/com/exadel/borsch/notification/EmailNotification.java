1f8ec3ca721cba4afd9c084e5b56c1b1923d749c
kabbi <userdima@gmail.com>
2013-07-25 19:46:28 +0300

    Made mailer work

--- borsch-core/src/main/java/com/exadel/borsch/notification/EmailNotification.java
+++ borsch-core/src/main/java/com/exadel/borsch/notification/EmailNotification.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
@@ -25,12 +26,10 @@ public class EmailNotification extends Notification {
     private static final String MAILER_PROPERTY_FILE = "/mailer.properties";
 
     private Properties properties;
-
-    @Value("${mailer.from}")
     private String from;
-
-    @Value("${mailer.subject}")
     private String subject;
+    private String username;
+    private String password;
 
     public EmailNotification(String message) {
         super(message);
@@ -38,6 +37,11 @@ public class EmailNotification extends Notification {
         try {
             Resource resource = new ClassPathResource(MAILER_PROPERTY_FILE);
             properties = PropertiesLoaderUtils.loadProperties(resource);
+
+            from = properties.getProperty("mailer.from");
+            subject = properties.getProperty("mailer.subject");
+            username = properties.getProperty("mailer.username");
+            password = properties.getProperty("mailer.password");
         } catch (IOException ex) {
             LOGGER.log(Level.SEVERE, "Cannot load mailer properties", ex);
         }
@@ -70,7 +74,13 @@ public class EmailNotification extends Notification {
     @Override
     public void submit(User target) {
         try {
-            Session session = Session.getDefaultInstance(properties);
+            Session session = Session.getDefaultInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
+            
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(target.getEmail()));
