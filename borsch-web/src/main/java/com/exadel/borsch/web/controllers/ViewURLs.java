b0033c7178d252208958f59f4ebab1463a293560
VladAbramov <abramov.vlad182@gmail.com>
2013-07-29 08:59:04 +0300

    Create orders page

--- borsch-web/src/main/java/com/exadel/borsch/web/controllers/ViewURLs.java
+++ borsch-web/src/main/java/com/exadel/borsch/web/controllers/ViewURLs.java
@@ -9,11 +9,14 @@ package com.exadel.borsch.web.controllers;
  * @author Vlad
  */
 public final class ViewURLs {
+
     public static final String LOGIN_PAGE = "login";
     public static final String HOME_PAGE = "home";
     public static final String MENU_PAGE = "menu";
     public static final String USERS_PAGE = "users";
+    public static final String ORDERS_PAGE = "orders";
     public static final String USER_EDIT_PAGE = "userEdit";
+
     private ViewURLs() {
     }
 }
