dfef3331adc770dbf6be31def363895aad6c807d
VladAbramov <abramov.vlad182@gmail.com>
2013-08-08 14:46:22 +0300

    Edit sql script

--- sql/script.sql
+++ sql/script.sql
@@ -24,16 +24,6 @@ CREATE  TABLE `borsch`.`Order` (
     FOREIGN KEY (`owner` )
     REFERENCES `borsch`.`user` (`idUser` ));
 
-CREATE  TABLE `borsch`.`Dish` (
-  `idDish` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(255) NOT NULL ,
-  `photoUrl` VARCHAR(255) NOT NULL ,
-  `price` INT NOT NULL ,
-  `description` VARCHAR(255) NOT NULL ,
-  `course` VARCHAR(255) NOT NULL ,
-  PRIMARY KEY (`idDish`) ,
-  UNIQUE INDEX `idDish_UNIQUE` (`idDish` ASC) );
-
 CREATE  TABLE `borsch`.`MenuItem` (
   `idMenuItem` INT NOT NULL AUTO_INCREMENT ,
   `date` DATETIME NOT NULL ,
@@ -45,18 +35,6 @@ CREATE  TABLE `borsch`.`MenuItem` (
     FOREIGN KEY (`idOrder` )
     REFERENCES `borsch`.`order` (`idOrder` ));
 
-CREATE  TABLE `borsch`.`Choices` (
-  `idMenuItem` INT NOT NULL ,
-  `idDish` INT NOT NULL ,
-  INDEX `idMenuItem_idx` (`idMenuItem` ASC) ,
-  INDEX `idDish_idx` (`idDish` ASC) ,
-  CONSTRAINT `idMenuItem`
-    FOREIGN KEY (`idMenuItem` )
-    REFERENCES `borsch`.`menuitem` (`idOrder` ),
-  CONSTRAINT `idDish`
-    FOREIGN KEY (`idDish` )
-    REFERENCES `borsch`.`dish` (`idDish` ));
-
 CREATE  TABLE `borsch`.`PriceList` (
   `idPriceList` INT NOT NULL AUTO_INCREMENT ,
   `creationTime` DATETIME NOT NULL ,
@@ -64,21 +42,27 @@ CREATE  TABLE `borsch`.`PriceList` (
   PRIMARY KEY (`idPriceList`) ,
   UNIQUE INDEX `idPriceList_UNIQUE` (`idPriceList` ASC) );
 
-CREATE  TABLE `borsch`.`Dishes` (
-  `idPriceList` INT NOT NULL ,
-  `dishId` INT NOT NULL ,
-  INDEX `idPriceList_idx` (`idPriceList` ASC) ,
-  INDEX `dishId_idx` (`dishId` ASC) ,
-  CONSTRAINT `idPriceList`
-    FOREIGN KEY (`idPriceList` )
-    REFERENCES `borsch`.`pricelist` (`idPriceList` ),
-  CONSTRAINT `dishId`
-    FOREIGN KEY (`dishId` )
-    REFERENCES `borsch`.`dish` (`idDish` ));
+CREATE  TABLE `borsch`.`Dish` (
+  `idDish` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(255) NOT NULL ,
+  `photoUrl` VARCHAR(255) NOT NULL ,
+  `price` INT NOT NULL ,
+  `description` VARCHAR(255) NOT NULL ,
+  `course` VARCHAR(255) NOT NULL ,
+  `order` INT NOT NULL ,
+  `priceList` INT NOT NULL ,
+  PRIMARY KEY (`idDish`) ,
+  UNIQUE INDEX `idDish_UNIQUE` (`idDish` ASC), 
+  CONSTRAINT `order`
+    FOREIGN KEY (`order` )
+    REFERENCES `borsch`.`order` (`idOrder` ),
+  CONSTRAINT `priceList`
+    FOREIGN KEY (`priceList` )
+    REFERENCES `borsch`.`PriceList` (`idPriceList` ));
 
 INSERT INTO `borsch`.`User` (`login`,`name`,`email`,`needEmailNotification`,`accessRights`,`locale`) 
 VALUES ("admin","Administrator The Great"
 		,"borschmail@gmail.com"
 		,true
 		,"[ROLE_EDIT_MENU_SELF, ROLE_EDIT_MENU_OTHER, ROLE_EDIT_PRICE, ROLE_PRINT_ORDER, ROLE_EDIT_PROFILE]"
-		,"en_US")
+		,"en_US");
