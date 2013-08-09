e6d02f6bd06c17a4f563983e14d2347f81dced39
VladAbramov <abramov.vlad182@gmail.com>
2013-08-09 16:50:41 +0300

    Replaces

--- borsch-core/src/main/java/com/exadel/borsch/managers/impl/simple/SimpleOrderChangeManager.java
+++ borsch-core/src/main/java/com/exadel/borsch/managers/impl/simple/SimpleOrderChangeManager.java
@@ -1,4 +1,4 @@
-package com.exadel.borsch.managers.simple;
+package com.exadel.borsch.managers.impl.simple;
 
 import com.exadel.borsch.entity.OrderChange;
 import com.exadel.borsch.managers.OrderChangeManager;
